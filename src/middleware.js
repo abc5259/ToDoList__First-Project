c50a628fe7baa637942d72e38691934403c9f5eb
LeeJaeHoon <dlwogns3413@naver.com>
2021-11-18 22:13:05 +0900

    fix middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
-  const { loggedIn, user } = req.session;
   res.locals.siteName = "ToDo";
-  res.locals.loggedIn = loggedIn;
-  res.locals.loggedInUser = user;
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.loggedInUser = req.session.user || {};
   next();
 };
