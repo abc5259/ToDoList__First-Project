6f2d10ddf8da9b410a2fd583b869fe675bb72340
LeeJaeHoon <dlwogns3413@naver.com>
2021-11-24 00:05:36 +0900

    clear sideScoll

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     main: "./src/client/js/main.js",
     createBoard: "./src/client/js/createBoard.js",
     createList: "./src/client/js/createList.js",
-    createCard: "./src/client/js/createCard.js",
+    sideScroll: "./src/client/js/sideScroll.js",
   },
   mode: "development",
   watch: true,
